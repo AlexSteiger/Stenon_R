dcf12126585caa8b27a31db0dbc063b9465d00d0
AlexSteiger <47745116+AlexSteiger@users.noreply.github.com>
2023-04-30 18:22:10 +0200

    k-means clustering added

--- stenon.r
+++ stenon.r
@@ -8,9 +8,11 @@ library(rgdal)
 library(gstat) # IDW
 
 # vorher "farmlab export Uni Rostock GG.csv" mit Notepad++ in UTF-8 umwandeln
-input <- read.csv2("farmlab export Uni Rostock GG.csv", sep =";", dec= ",", header = TRUE)
+input <- read.csv("input/farmlab export Uni Rostock GG(3).csv", sep =",", dec= ".", header = TRUE)
+field_name <- input[1,4]
+print(field_name)
 
-df <- input[, c(2,4,5,7,10,11,12,13,14,23)]
+df <- input[, c(3,5,6,8,14,16,18,20,22,28,37,30)]
 str(df)
 df <- df %>% 
   rename(
@@ -18,10 +20,14 @@ df <- df %>%
     P = P..mg.100g.,
     K = K..beta...mg.100g.,
     MC  = Moisture....,
-    SOC = SOC....
+    SOC = SOC....,
+    pH = pH.,
+    Mg = Mg..mg.100g.,
+    Soil_Temp = Soil.Temperature...C.,
+    texture = Soil.texture.class.
   )
 
-df[, 2:10] <- lapply(df[, 2:10], as.numeric)
+df[, 2:11] <- lapply(df[, 2:11], as.numeric)
 
 # Add Date column
 df$date <- as.Date(df$timestamp)
@@ -29,13 +35,13 @@ df$date <- as.Date(df$timestamp)
 str(df)
 
 ##############################################################################
-## Calculate the summaries of each measurement series
+## Boxplots
 
 # Group the data by date and
 # calculate the mean and standard deviation for column 4 to 12 for each date
 df_summary <- df %>% 
   group_by(date) %>% 
-  summarise_at(vars(4:10), list(mean=mean, sd=sd))
+  summarise_at(vars(4:11), list(mean=mean, sd=sd))
 
 str(df_summary)
 
@@ -44,9 +50,10 @@ plot_boxplot <- function(data, x, y, title, ylab){
     geom_boxplot(outlier.size=0.5) +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
-    labs(title = title, y = ylab) +
+    labs(title = title, y = ylab, x = "") +
     scale_x_date(date_breaks = "1 months") + 
-    scale_x_date(date_labels = "%b-%Y")
+    scale_x_date(date_labels = "%b-%Y") +
+    theme(axis.text.x = element_text(size=8,angle=45,vjust=1,hjust=1))
 }
 
 p_N   <- plot_boxplot(df,date,Nmin,"Nmin","Nmin [kg/ha]")
@@ -55,56 +62,114 @@ p_K   <- plot_boxplot(df,date,K,"K - Potassium","K [mg/100g]")
 p_SOC <- plot_boxplot(df,date,SOC,"SOC","Soil Organic Carbon [%]")
 p_MC  <- plot_boxplot(df,date,MC,"Moisture Content","MC [%]")
 p_pH  <- plot_boxplot(df,date,pH,"pH","pH")
+p_Mg  <- plot_boxplot(df,date,Mg,"Mg","Magnesium [mg/100g]")
+p_Tmp <- plot_boxplot(df,date,Soil_Temp,"Soil Temperature","Temperature [°C]")
 
-( p_N  | p_P  | p_K) /
-( p_SOC | p_MC | p_pH)
 
-#(p_N | p_P) /
-#(p_K | p_SOC) /
-#(p_MC| p_pH)
+(p_N|p_P|p_K|p_Mg) / (p_SOC|p_MC|p_pH|p_Tmp)
+p <- (p_N|p_P|p_K|p_Mg) / (p_SOC|p_MC|p_pH|p_Tmp)
+ggsave(paste0("Boxplot_",field_name,".png"), p,width=9,height=5.5)
 
+##############################################################################
+## Z-Maps
+
+# Normalization
 df_z <- df %>%
   group_by(date) %>%
-  mutate_at(vars(4:9), ~scale(.)) %>%
+  mutate_at(vars(4:11), ~scale(.)) %>%
   ungroup() %>%
-  mutate(across(4:9, round, digits = 2))
+  mutate(across(4:11, round, digits = 2))
 
-#df_z <- df_z %>% 
-#  mutate(across(4:9, ~ifelse(abs(.) > 2.5, NA, .)))
+df_z_outliers <- df_z %>%
+  filter(rowSums(abs(.[4:10]) > 2.5) > 2) 
 
-# Filter for rows where less than 2 values are greater than 2.5
+# Filter out rows where more than 2 values are greater than 2.5
 df_z <- df_z %>%
   filter(rowSums(abs(.[4:10]) > 2.5) < 2) 
 
 print(paste0(nrow(df)-nrow(df_z)," from ",nrow(df)," deleted"))
 
-dev.off()
+dev.off() #sometimes ggplot doesn't work without this
 
-plot_map <- function(data, x, y, color, colors, size = 2){
+plot_map <- function(data, x, y, color){
   ggplot(data = data, aes(x = {{x}}, y = {{y}}, color = {{color}})) +
-    geom_point(size = size) +
-    scale_color_gradientn(colors = colors) +
-    labs(title = "", y = "", x = "") +
-    theme_classic()
+    geom_point(size = 1) +
+    scale_color_gradientn(colors = rainbow(5)) +
+    labs(title = "") +
+    theme_classic() +
+    theme(axis.title.x = element_blank()) +
+    theme(axis.title.y = element_blank()) +
+    theme(axis.text.x = element_text(size=6)) +
+    theme(axis.text.y = element_text(size=6,angle=90,hjust=0.5))
 }
 
-map_N <- plot_map(df_z,long,lat,color=Nmin,rainbow(4))
-map_P <- plot_map(df_z,long,lat,color=P,rainbow(4))
-map_K <- plot_map(df_z,long,lat,color=K,rainbow(4))
-map_SOC <- plot_map(df_z,long,lat,color=SOC,rainbow(4))
-map_MC <- plot_map(df_z,long,lat,color=MC,rainbow(4))
-map_pH <- plot_map(df_z,long,lat,color=pH,rainbow(4))
+map_N   <- plot_map(df_z,long,lat,color=Nmin)
+map_P   <- plot_map(df_z,long,lat,color=P)
+map_T   <- plot_map(df_z,long,lat,color=texture)
+map_SOC <- plot_map(df_z,long,lat,color=SOC)
+map_MC  <- plot_map(df_z,long,lat,color=MC)
+map_pH  <- plot_map(df_z,long,lat,color=pH)
+map_Mg  <- plot_map(df_z,long,lat,color=Mg)
+map_K   <- plot_map(df_z,long,lat,color=K)
 
-( map_N  | map_P  | map_K) /
-( map_SOC | map_MC | map_pH)
+(map_N|map_P|map_K|map_Mg)/( map_SOC|map_MC|map_pH|map_T)
+p_map <- (map_N|map_P|map_K|map_Mg)/( map_SOC|map_MC|map_pH|map_T)
+ggsave(paste0("zmap_",field_name,".png"),p_map,width=10,height=3.5)
+
+##############################################################################
+## k-means clustering
+
+#Plot all the data, to see if there are some relations between them
+plot(df_z[,c(4:11)])
+
+df_k <- df_z[,c(4:11)]
+library (hopkins)
+
+# Hopkins Test to test the probability that the data has a uniform random distribution 
+# Lower is better, e.g. 0.5 indicates high degree of spatial randomness
+hopkins <- hopkins (df_k, n=nrow(df_k)-1)
+print(hopkins) 
+
+# Transform df into a spatial points dataframe
+#coordinates(df_k) <- c("long", "lat")
+
+vars <- df_k[, c("MC","pH","K","Mg","texture")]
+
+k <- kmeans(vars, 4)
+
+df_z$cluster <- k$cluster
+
+# Calculate Silhouette Width (The higher the better)
+library(cluster)
+sil_width <- silhouette(df_k$cluster, dist(vars))
+cat("Silhouette Width:", mean(sil_width[,3]), "\n")
+
+# Calculate Calinski-Harabasz Index
+#library(fpc)
+#ch <- calinski_harabasz(data[,2:6], data$cluster)
+
+# Print the validation measures
+cat("Within-cluster Sum of Squares (WSS):", sum(wss), "\n")
+
+
+print(sil_width)
+
+ggplot(df_z, aes(x = long, y = lat, color = as.factor(cluster))) +
+  geom_point(size=3) +
+  labs(title = "Spatial Clustering with K-means", color = "Cluster") +
+  theme_bw()
+
+#Nmin + SOC = 0.33
+#"K","MC","pH" = 0.32
+#MC + pH = 0.36
 
 # Make a SpatialPointsDataFrame
 data        <- df_z[ , c(4:12)]
 coords      <- df_z[ , c("long", "lat")]
 crs         <- CRS("+init=epsg:4326") # => [+proj=longlat +datum=WGS84]
 spdf <- SpatialPointsDataFrame(coords             = coords,
-                                      data        = data, 
-                                      proj4string = crs)
+                               data        = data, 
+                               proj4string = crs)
 
 plot(spdf)
 
@@ -123,12 +188,6 @@ fullgrid(grd)    <- TRUE  # Create SpatialGrid object
 proj4string(grd) <- proj4string(sensor.spdf)
 
 spdf <- spTransform(spdf, CRS=CRS("+init=epsg:32623"))
-
-
-
-
-
-
 
 
 
@@ -151,4 +210,3 @@ spdf <- spTransform(spdf, CRS=CRS("+init=epsg:32623"))
 #    scale_x_date(date_labels = "%b-%Y")
 #  p
 #}
-  
